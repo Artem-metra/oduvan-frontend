87e5020554ccd4f270f40b1ef570d20dc85ca67e
sovadeveloper <sovadeveloper@mail.ru>
2021-07-23 15:29:10 +0300

    123

--- maps/MapProduct.py
+++ maps/MapProduct.py
@@ -33,8 +33,14 @@ def api_products_sort_from_max_to_min():
 
 @product_app.route('/api/product/get')
 def api_product_get():
-    pass
-    return utils.complete_request(request, request.path)
+    params = {}
+    for item in request.args:
+        params[item] = request.args.get(item)
+    params['user_id'] = session['user_id']
+    params['basket_id'] = session['basket_id']
+    response = utils.complete_request_with_parameters(params, request.path)
+    print(response)
+    return response
 
 
 @product_app.route('/api/product/edit')
@@ -103,3 +109,23 @@ def api_product_remove_image():
 @product_app.route('/api/products/edit_image', methods=['POST'])
 def api_user_edit_image():
     return utils.complete_request_post(json.loads(request.data), request.path)
+
+
+@product_app.route('/api/max_price_for_category')
+def api_max_price_for_category():
+    params = {}
+    for item in request.args:
+        params[item] = request.args.get(item)
+    response = utils.complete_request_with_parameters(params, request.path)
+    print(response)
+    return response
+
+
+@product_app.route('/api/min_price_for_category')
+def api_min_price_for_category():
+    params = {}
+    for item in request.args:
+        params[item] = request.args.get(item)
+    response = utils.complete_request_with_parameters(params, request.path)
+    print(response)
+    return response
